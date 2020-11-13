9ae021d04fc9a2b1376604f01ec5933e731931f2
Harshet Dayasankar <hdayasankar@opentext.com>
2020-11-13 17:34:05 +0530

    Form Built with 2 way data binding

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-form',
@@ -7,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  OriginalUserSettings: UserSettings = {
+    name: 'Milton',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    SubscriptionType: 'Lifetime',
+    notes: 'Im Notes'
+  };
+
+  UserSettings: UserSettings = {...this.OriginalUserSettings};
   constructor() { }
 
   ngOnInit(): void {
